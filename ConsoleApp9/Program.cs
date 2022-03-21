71dc39dbc65364e41068535e5ad9074b8ff4b8f2
wiolta <102069943+wiolta@users.noreply.github.com>
2022-03-21 20:07:39 +0100

    Update Program.cs

--- ConsoleApp9/Program.cs
+++ ConsoleApp9/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp9
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello !");
         }
     }
 }
