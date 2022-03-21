a5a9cc1c938e249c0570df36a2a36d678ff70238
Wioleta Ligęza <wiolta92@gmail.com>
2022-03-21 22:25:14 +0100

    komit lok

--- ConsoleApp9/Program.cs
+++ ConsoleApp9/Program.cs
@@ -13,7 +13,7 @@ namespace ConsoleApp9
             Console.WriteLine("local");
             Console.WriteLine("popik");
             Console.WriteLine("lokalne");
-            Console.WriteLine("rozwiazanie");
+            Console.WriteLine("lokalne");
         }
     }
 }
