b33d1e23392984b3568945f8b0aa5b591c6198db
Wioleta Ligęza <wiolta92@gmail.com>
2022-03-21 22:37:11 +0100

    sssss

--- ConsoleApp9/Program.cs
+++ ConsoleApp9/Program.cs
@@ -13,7 +13,7 @@ namespace ConsoleApp9
             Console.WriteLine("local");
             Console.WriteLine("popik");
             Console.WriteLine("lokalne");
-            Console.WriteLine("lokes");
+            Console.WriteLine("lokessssssssssssssssssssss");
         }
     }
 }
